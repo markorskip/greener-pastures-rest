dc9263b5d43507350a3117b8a3504a2e0ad9cc15
Mark Sorenson <marksorenson@Marks-MacBook-Pro.local>
2020-07-12 05:57:39 -0500

    implemented caching on the bestOpportunityService

--- src/main/java/io/greenerpastures/rest/bestopportunity/service/BestOpportunityService.java
+++ src/main/java/io/greenerpastures/rest/bestopportunity/service/BestOpportunityService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Optional;
 
 @Component
@@ -15,9 +16,13 @@ public class BestOpportunityService {
 
     private CostOfLivingService costOfLivingService;
 
+    private HashMap<OpportunityInputs, OpportunityResults> cache;
+
+
     @Autowired
     public BestOpportunityService(CostOfLivingService costOfLivingService) {
         this.costOfLivingService = costOfLivingService;
+        this.cache = new HashMap<>();
     }
 
 
@@ -25,20 +30,38 @@ public class BestOpportunityService {
     // service? Or should the controller contact the multiples
     public OpportunityResults calculate(OpportunityInputs inputs) {
 
+        OpportunityResults cacheResults = checkCache(inputs);
+        if (cacheResults != null) return cacheResults;
+
         OpportunityResults results = new OpportunityResults(inputs);
 
         // First calculate taxes owed,
-        results.setAnnualTaxesOwed(TaxesOwedService.calculateAnnualTaxesOwed(inputs));
+        TaxesOwedService taxesOwedService = new TaxesOwedService();
+        results.setAnnualTaxesOwed(taxesOwedService.calculateAnnualTaxesOwed(inputs));
 
         // Second calculate Cost of Living
         results.setStateCostOfLiving(costOfLivingService.calculateCostOfLivingBasedOnStateIntitials(inputs.getState()));
+        addToCache(inputs, results);
 
         return results;
     }
 
-    private BigDecimal computeAdjustedPay(BigDecimal afterTaxPay, BigDecimal valueOfADollar) {
-        return afterTaxPay.multiply(valueOfADollar);
+    private OpportunityResults checkCache(OpportunityInputs inputs) {
+        if (cache.containsKey(inputs)) {
+            System.out.println("Utilizing cache for performance optimization");
+            return cache.get(inputs);
+        }
+        return null;
     }
 
+    private void addToCache(OpportunityInputs inputs, OpportunityResults results) {
+        cache.put(inputs,results);
+        System.out.println("Adding new result to cache. Size is now: " + cache.size());
+
+        if (cache.size() > 10000) {
+            System.out.println("Cache max size exceeded.  Clearing cache.");
+            cache.clear();
+        }
+    }
 
 }
--- src/main/java/io/greenerpastures/rest/bestopportunity/service/TaxesOwedService.java
+++ src/main/java/io/greenerpastures/rest/bestopportunity/service/TaxesOwedService.java
@@ -8,10 +8,14 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+
 
 public class TaxesOwedService {
 
-    public static AnnualTaxesOwed calculateAnnualTaxesOwed(OpportunityInputs inputs) {
+    private HashMap<String, String> localCache;
+
+    public AnnualTaxesOwed calculateAnnualTaxesOwed(OpportunityInputs inputs) {
         String uri = "https://taxee.io/api/v2/calculate/2020";
         String auth = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBUElfS0VZX01BTkFHRVIiLCJodHRwOi8vdGF4ZWUuaW8vdXNlcl9pZCI6IjVlMGNiMzE1MGM1ZDE5MjkxMWQzNDg1MiIsImh0dHA6Ly90YXhlZS5pby9zY29wZXMiOlsiYXBpIl0sImlhdCI6MTU3Nzg5MDU4MX0.-gjctbfrZpR0Hw3C-CavZNEGAl2-890FJSG5TSml3i0";
 
@@ -36,4 +40,6 @@ public class TaxesOwedService {
 
         return response.getBody().getTaxesOwed();
     }
+
+
 }
